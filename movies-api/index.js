99d33b2122447ffcd8ff2d4481b244a9469af6b2
Jiaqi Gu <919659233@qq.com>
2020-12-02 23:02:36 +0000

    added routing for add,update,delete movie

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import bodyParser from 'body-parser';
 import moviesRouter from './api/movies';
+
 
 dotenv.config();
 
@@ -9,6 +11,9 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.static('public'));
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use('/api/movies', moviesRouter);
 
 app.listen(port, () => {
